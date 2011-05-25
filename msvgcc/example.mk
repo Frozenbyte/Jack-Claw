691af82e576e828b565e99753cf1f34a35102081
Turo Lamminen <turotl@gmail.com>
2011-05-25 14:04:58 +0300

    Add support for msvgcc hack
    Enable building with msvgcc msvc-on-Linux hack
    http://github.com/turol/msvgcc
    
    Does not work correctly yet because some includes in the
    source are not in the format msvgcc expects.

---
+++ msvgcc/example.mk
@@ -0,0 +1,104 @@
+# example of local configuration
+# copy to local.mk
+
+
+# this is for building with msvgcc
+# The glorious and horrible Visual-C-on-Linux hack
+# http://github.com/turol/msvgcc
+
+
+# location of source
+TOPDIR:=..
+
+
+# compiler options etc
+CXX:=./msvgcc
+
+# link time optimization, gcc >4.6 required
+LTO:=n
+
+
+DIRECT3D:=y
+OPENGL:=y
+# null fmod openal
+SOUND:=fmod
+
+# default to debugging enabled
+DEBUG:=y
+
+
+# optimization options
+OPTFLAGS:=
+
+
+# unlike gcc, msvc can't do debugging with optimization on
+ifeq ($(DEBUG),n)
+
+# /O1 minimize space
+# /O2 maximize speed
+# /Oi enable intrinsic functions
+# /GA optimize for Windows Application
+# /GF enable read-only string pooling
+# /fp:fast "fast" floating-point model; results are less predictable
+# /arch:<SSE|SSE2>
+OPTFLAGS+=/O1 /Oi /GA /GF /fp:fast /arch:SSE2
+
+endif
+
+
+CXXFLAGS:=
+CXXFLAGS+=-I$(TOPDIR) -I$(TOPDIR)/claw_proto -I$(TOPDIR)/storm/include -I$(TOPDIR)/storm/keyb3
+CXXFLAGS+=/FI$(TOPDIR)/claw_proto/configuration.h
+CXXFLAGS+=-DLIGHT_MAX_AMOUNT=5
+
+# enable exceptions or the compiler complains about its standard headers
+CXXFLAGS+=/EHsc
+
+CXXFLAGS+=-D_CRT_SECURE_NO_DEPRECATE
+
+
+# linker settings
+LDFLAGS:=/subsystem:windows
+LDLIBS:=winmm.lib comdlg32.lib gdi32.lib
+LDLIBS+=PhysXLoader.lib
+LDLIBS+=$(TOPDIR)/storm/lib/minizip.lib
+LDLIBS+=$(TOPDIR)/storm/lib/zlib.lib
+LDLIBS_d3d:=d3d9.lib d3dx9_33.lib dinput8.lib dxguid.lib vfw32.lib
+LDLIBS_opengl:=glew32.dll opengl32.lib glu32.lib SDL.lib SDL_image.lib SDL_ttf.lib
+
+LDLIBS_fmod:=fmod.lib
+LDLIBS_openal:=boost-thread.lib
+
+
+OBJSUFFIX:=.obj
+EXESUFFIX:=.exe
+
+
+PHYSX_VERSION:=2.8.3
+PHYSX_LIBDIR:=/usr/lib/PhysX
+PHYSX_INCDIR:=/usr/include/PhysX
+
+PHYSX_PARTS:=Physics Cooking NxExtensions NxCharacter Foundation PhysXLoader
+
+PHYSX_INCLUDE_DIRS:=$(foreach dir,$(PHYSX_PARTS), $(PHYSX_INCDIR)/v$(PHYSX_VERSION)/SDKs/$(dir)/include)
+LPHYSX:=-L$(PHYSX_LIBDIR)/v$(PHYSX_VERSION)
+
+IPHYSX:=$(foreach dir,$(PHYSX_INCLUDE_DIRS),-I$(dir))
+
+
+# different libs required for debugging...
+ifeq ($(DEBUG),y)
+
+CXXFLAGS+=/MTd -D_DEBUG
+# /RTC1 Enable fast checks (/RTCsu)
+# /Z7 enable old-style debug info
+CXXFLAGS+=/RTC1 /Z7
+LDFLAGS+=/nodefaultlib:msvcrt.lib
+
+else
+
+CXXFLAGS+=/MT -DNDEBUG
+LDLIBS+=libcmt.lib
+LDFLAGS+=/nodefaultlib:msvcrt.lib
+
+endif
